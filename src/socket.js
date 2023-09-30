fbc5a693eb67b6c806a573a4f32a911d86043b14
rgiri1706 <rgiri1706@gmail.com>
2023-09-30 08:36:49 +0530

    Mentioned endpoint for prod

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io('http://localhost:5000');
+const url = process.env.NODE_ENV === 'production' ? 'https://sketchpad-server.onrender.com' : 'http://localhost:5000'
+
+export const socket = io(url);
